e968cdf9a116e9413f56ff6afad21f41e03f986c
Theo Prince <chamtail@126.com>
2024-09-12 02:41:53 +0800

    initialize

---
+++ src/main/java/vip/theo/concrastinate/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package vip.theo.concrastinate.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String hello() {
+        return "hello";
+    }
+}
